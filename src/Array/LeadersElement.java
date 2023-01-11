f541384984994a01b3f3c448a484a04706ad04a3
aman-sunnuy <amanchaurasiya786@gmail.com>
2023-01-11 21:32:52 +0530

    leaders in n complextiy

--- src/Array/LeadersElement.java
+++ src/Array/LeadersElement.java
@@ -2,11 +2,15 @@ package Array;
 
 public class LeadersElement {
     public static void main(String[] args) {
-        int[] arr={10,20,30,40,50};
+        int[] arr={10,20,30,60,50};
         int n= arr.length;
         leaders(arr,n);
+        System.out.println();
+        leaders2(arr,n);
 
     }
+
+    // leaders in array in [O(N*N)] complexity
     static void leaders(int arr[],int n){
         for(int i=0;i<n;i++){
             boolean flag=false;
@@ -17,7 +21,19 @@ public class LeadersElement {
                 }
             }
             if (flag==false){
-                System.out.println(arr[i]);
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+
+    //  leaders in [O(n)] complexity
+    static void leaders2(int arr[],int n){
+        int curr_ldr=arr[n-1];
+        System.out.print(curr_ldr+" ");
+        for (int i=n-2;i>=0;i--){
+            if(arr[i]>curr_ldr){
+                curr_ldr=arr[i];
+                System.out.print(arr[i]+" ");
             }
         }
     }
